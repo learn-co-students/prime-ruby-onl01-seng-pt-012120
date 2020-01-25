bf551fd3a7a6f21f1535bead8def3da13cd9ebcf
Dan Foley <dan@dan-foley.com>
2020-01-25 16:36:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 # Add  code here!
 def prime?(num)
   # check if number is divisible by 2,3,5,7
+  if num <= 1
+    return false
+  elsif (2...num).any? {|i| num % i == 0}
+    return false
+  else
+    return true
+  end
 end
