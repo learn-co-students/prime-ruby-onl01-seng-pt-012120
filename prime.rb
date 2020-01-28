245d6687ae73a197d06ceb50c4c67e947c1ee22e
James Rogers <jarogers095@gmail.com>
2020-01-28 07:48:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  result = true
+  if num < 2 
+    result = false
+  elsif num == 2
+    result = true
+  else
+    range = (2..(num / 2)).to_a
+    range.each do |divisor|
+      if num % divisor == 0
+        result = false
+      end
+    end
+  end
+  
+  return result
+end
