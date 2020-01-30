f1b544daac0b8a1ad51773588bf6cfac8fc50256
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-30 07:26:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(n)
+
+  if n <= 1
+    return false
+  end
+
+  i = 2
+  while i < n do
+    if n%i === 0
+      return false
+    end
+    i += 1
+  end
+
+  return true
+
+end
