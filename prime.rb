431caa06d464552d17fac46d853d2e362d53c8f7
Timur Mishiev <timurmishiev@gmail.com>
2020-01-31 00:35:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+=begin
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+end 
+end
+=end
+
+def prime?(num)
+  if num <= 1 || num == 0 || num == 1
+    return false
+  elsif
+    (2..num - 1).each do |i|
+      if num % i == 0
+      return false
+    end
+  end
+end
+true  
+end
