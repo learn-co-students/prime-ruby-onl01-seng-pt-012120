3fe3214efb4fc5d0549c3fcc5d8be7894240e2d0
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 08:10:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    new_number = (start..number-1).to_a
+    new_number.none? do |num_to_test| 
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
