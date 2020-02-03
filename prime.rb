6a5298b478489409c348624faaed080e36ceab77
Patricia C <patricia@patriciaco.com>
2020-02-03 07:17:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+# # Add  code here!
+
+def prime?(number)
+  if number <= 1
+    false
+    elsif number == 2 
+    true 
+  else 
+    (2..number/2).each do |n| #(2..number-1)
+    return false if number % n == 0 
+    end 
+    true
+  end  
+end 
