fb27d999d61cc46264a5d0186b6f13c53e005ca2
Chris Beyer <beyercorpuz@gmail.com>
2020-02-04 01:34:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -9,8 +9,22 @@
 #   true
 # end
 
-def prime(n)
-  return false if n < 2
+# def prime(n)
+#   return false if n < 2
 
-  (2..n/2).none?{|i| n % i == 0}
+#   (2..n/2).none?{|i| n % i == 0}
+# end
+
+ef get_prime_no_upto(number)
+  start = 2
+  primes = (start..number).to_a
+  (start..number).each do |no|
+    (start..no).each do |num|
+      if ( no % num  == 0) && num != no
+        primes.delete(no)
+        break
+      end
+    end
+  end
+  primes
 end
